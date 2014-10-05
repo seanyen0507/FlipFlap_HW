ebcf1f5e97b1fa5765fd086a48ab6cc9139af185
Sean Hsieh <seanyen0507@gmail.com>
2014-10-05 16:15:07 +0800

    modified some files

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,7 +1,20 @@
 require_relative 'tsv_buddy'
+require 'yaml'
 
 class FlipFlap
+  include TsvBuddy
+  #attr_accessor :data
 
   # Do not create any initializers
+  def take_yaml(yml)
+    @data = YAML::load(yml)
+    @data
+
+  end
+  def to_yaml
+    @data.to_yaml
+
+  end
+
 
 end
